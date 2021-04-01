2905849708ef64781a29716878bee679160a81c9
hisscaredbrain <hsscaredbrain@gmail.com>
2021-04-01 09:51:01 +0200

    Practice assignemnt for Week2

--- myfunction.R
+++ myfunction.R
@@ -6,3 +6,22 @@ myfunction <- function(x) {
 second <- function(x) {
 	x + rnorm(length(x))
 }
+
+f <- function(x) {
+  g <- function(y){
+    y+z
+  }
+  z <- 4
+  x + g(x)
+}
+
+weightmedian <- function(directory, day) {
+  files_list <- list.files(directory, full.names = TRUE) # Creates a list of files
+  dat <- data.frame() # Create an empty dataframe
+  for (i in 1:length(files_list)) {
+    # Loop through files and rbind them
+    dat <- rbind(dat, read.csv(files_list[i]))
+  }
+  dat_subset <- dat[which(dat[, "Day"] == day), ] # create a subset for the day
+  median(dat_subset[, "Weight"], na.rm = TRUE) # compute median while ignoring NA
+}
